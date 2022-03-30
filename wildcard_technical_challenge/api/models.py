c57ab7a61338fb3e8568dc129972376cc0ee385f
Eric Lee <eric@clearinterface.com>
2022-03-30 15:36:08 -0500

    added initial model migration

--- wildcard_technical_challenge/api/models.py
+++ wildcard_technical_challenge/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class CensoredWords(models.Model):
+    word_identifier = models.CharField(max_length=100, blank=False, null=False)
+    word_list = models.TextField(blank=False)
+    date_added = models.DateField()
+    active = models.BooleanField(default=True)
