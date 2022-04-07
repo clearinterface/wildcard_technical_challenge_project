a060c3e6b72c16aafb6f7a0add3875445fa8b3f8
Eric Lee <eric@clearinterface.com>
2022-04-07 01:29:14 -0500

    fix bug in words with apostrophe

--- wildcard_technical_challenge/api/utils.py
+++ wildcard_technical_challenge/api/utils.py
@@ -2,18 +2,18 @@ import re
 
 
 def extraction_of_words(word_list):
+    #use regular expression to check for apostrophe and substitute it with ^
+    # (?<=[A-Za-z])'(?=[A-Za-z])
+    word_list = re.sub(r"(?<=[A-Za-z])'(?=[A-Za-z])", "^", word_list)
     word_list = word_list.replace("'",'"').replace('\\','')
     quoted_phrases = word_list.split('"')[1::2]
     if len(quoted_phrases) > 0:
         for q in quoted_phrases:
             word_list = word_list.replace(q, '').replace('"', '').strip()
-    # else:
-    #     quoted_phrases = word_list.split("'")[1::2]
-    #     for q in quoted_phrases:
-    #         word_list = word_list.replace(q, '').replace("'", '').strip()
+
 
     # replace word_list delimeter with pipe
-    word_list = word_list.replace(" ", "|").replace(",", "|")
+    word_list = word_list.replace(" ", "|").replace(",", "|").replace("^", "'")
     for w in word_list.strip().split("|"):
         if w != '':
             quoted_phrases.append(w)
